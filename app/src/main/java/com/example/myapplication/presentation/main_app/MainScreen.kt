cdb0b2d519cc70235b47a21627a0b88b6593bcda
cetvs <68225163+cetvs@users.noreply.github.com>
2022-07-04 16:01:35 +0300

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/presentation/main_app/MainScreen.kt
@@ -0,0 +1,11 @@
+package com.example.myapplication.presentation.main_app
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Preview()
+@Composable
+fun AppScreen() {
+
+}
